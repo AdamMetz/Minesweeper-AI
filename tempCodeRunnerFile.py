0431ca6f3dd6512750623f432bc68fa2ce13a851
AdamMetz <47069058+AdamMetz@users.noreply.github.com>
2022-05-26 21:38:18 -0600

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-        # if self.is_bomb:
-        #     window.blit(bomb_image, (self.x, self.y))
-        # else:
-        #     window.blit(pygame.image.load('game_images/'+str(self.nearby_bombs)+'.png'), (self.x, self.y))
